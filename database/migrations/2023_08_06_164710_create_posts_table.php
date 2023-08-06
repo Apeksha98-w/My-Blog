20318196f643682fa6384c90c93b2b95e8ca38e6
Apeksha <apekshawarnakulasooriya98@gmail.com>
2023-08-06 23:26:51 +0530

    Add columns and a foreign key to post table

--- database/migrations/2023_08_06_164710_create_posts_table.php
+++ database/migrations/2023_08_06_164710_create_posts_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('description');
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
